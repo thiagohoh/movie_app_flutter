1a1b12aec826e87e63011f7b405e84784a588ea5
Thiago <thiago14lost@gmail.com>
2023-01-09 17:54:00 -0300

    feat(component): create fullcard component. component for displaying movie image and name.

---
+++ lib/components/FullCard.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class FullCard extends StatelessWidget {
+  const FullCard(
+      {Key? key, required this.title, required this.imageWidget, this.onClick})
+      : super(key: key);
+
+  final String title;
+  final Widget imageWidget;
+  final Function? onClick;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: InkWell(
+        onTap: () {
+          onClick?.call();
+        },
+        child: Card(
+          borderOnForeground: true,
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20),
+              side: const BorderSide(color: Colors.red)),
+          shadowColor: Colors.redAccent,
+          child: SizedBox(
+            width: double.infinity,
+            height: double.infinity,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                ClipOval(
+                  child: SizedBox.fromSize(
+                    size: const Size.fromRadius(150),
+                    child: imageWidget,
+                  ),
+                ),
+                Text(
+                  title,
+                  style: const TextStyle(
+                      fontSize: 22, fontWeight: FontWeight.w500),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
