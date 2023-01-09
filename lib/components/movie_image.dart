b028c9e7b37d789991f8a1389a86318f9ce0a791
Thiago <thiago14lost@gmail.com>
2023-01-09 19:02:33 -0300

    feat(component): create movie_image component. component for displaying movie image and name.

---
+++ lib/components/movie_image.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MovieImage extends StatelessWidget {
+  const MovieImage({Key? key, required this.image}) : super(key: key);
+
+  final Widget image;
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipOval(
+      child: SizedBox.fromSize(
+        size: const Size.fromRadius(90),
+        child: image,
+      ),
+    );
+  }
+}
