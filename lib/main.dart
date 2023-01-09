e0ab3179866ab6ab136ea8537a187cae9303d219
Thiago <thiago14lost@gmail.com>
2023-01-09 19:43:15 -0300

    feat(env): add dotenv support. add flutter_dotenv refactor main to support dotenv initialization

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:appmoove_mobile_test/pages/dashboard.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+Future main() async {
+  await dotenv.load(fileName: ".env");
   runApp(const MovieApp());
 }
 
