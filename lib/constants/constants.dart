bbc6769434ea99230de0ad8676e0ecf666eedbd5
Thiago <thiago14lost@gmail.com>
2023-01-11 01:06:38 -0300

    feat(constants): add movies endpoint and popular movie endpoint constants.

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -2,6 +2,8 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 class Constants {
   static final apiKey = dotenv.env['api_key'];
   static const baseUrl = "https://api.themoviedb.org/3/";
+  static const movieEndpoint = "movie/";
+  static const popularMovieEndpoint= "movie/popular";
   static const trendingDayEndpoint = "trending/movie/day";
   static const trendingWeekEndpoint = "trending/movie/week";
   static const apiKeyParam = "?api_key=";
