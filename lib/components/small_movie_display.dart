28dc5fbd0c0778833daf8fe65dd891bb84167441
Thiago <thiago14lost@gmail.com>
2023-01-11 01:15:11 -0300

    feat(components): add onClick function to widget smallmoviedisplay
    smallmoviedisplay now has a third and optional param onClick

--- lib/components/small_movie_display.dart
+++ lib/components/small_movie_display.dart
@@ -2,23 +2,29 @@ import 'package:appmoove_mobile_test/components/movie_image.dart';
 import 'package:flutter/material.dart';
 
 class SmallMovieDisplay extends StatelessWidget {
-  const SmallMovieDisplay({Key? key, required this.title, required this.image})
+  const SmallMovieDisplay({Key? key, required this.title, required this.image, this.onClick})
       : super(key: key);
 
   final String title;
   final Widget image;
+  final Function? onClick;
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.spaceAround,
-      children: [
-        MovieImage(image: image),
-        Text(
-          title,
-          style: const TextStyle(fontSize: 12, fontWeight: FontWeight.w500),
-        )
-      ],
+    return InkWell(
+      onTap: () {
+        onClick?.call();
+      },
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          MovieImage(image: image),
+          Text(
+            title,
+            style: const TextStyle(fontSize: 12, fontWeight: FontWeight.w500),
+          )
+        ],
+      ),
     );
   }
 }
